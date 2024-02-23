63d782f21bab97a6ed67da8d1af8d76f7373db12
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-02-23 11:21:02 +0100

    Refactored handlebarsHelpers.js to include a conditional filter for checked todos

--- src/lib/handlebarsHelpers.js
+++ src/lib/handlebarsHelpers.js
@@ -1,17 +1,10 @@
 
 export default {
-    filterUncheckedTodos: (todos) => {
-        return todos.filter(todo => !todo.isCompleted);
+    filterUncheckedTodos: (todos, param) => {
+        if(param.hash.isChecked) {
+            return todos.filter(todo => todo.isCompleted);
+        } else {
+            return todos.filter(todo => !todo.isCompleted);
+        }
     },
-    filterCheckedTodos: (todos) => {
-        return todos.filter(todo => todo.isCompleted);
-    },
-    filterUniqueCategories: (todos) => {
-        const categories = todos.map(todo => todo.category);
-        const uniqueCategories = [...new Set(categories)];
-        uniqueCategories.sort();
-        uniqueCategories.splice(uniqueCategories.indexOf('General'), 1);
-        uniqueCategories.unshift('General');
-        return uniqueCategories;
-    }
 }
