9da54c6353cb569e6ba555946ed9c315afc9dd15
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-03-01 15:55:58 +0100

    Updated handlebar helper filterTodosByStatus

--- src/lib/handlebarsHelpers.js
+++ src/lib/handlebarsHelpers.js
@@ -2,9 +2,9 @@
 export default {
     filterTodosByStatus: (todos, param) => {
         if(param.hash.isChecked) {
-            return todos.filter(todo => todo.isCompleted);
+            return todos.filter(todo => todo.is_completed);
         } else {
-            return todos.filter(todo => !todo.isCompleted);
+            return todos.filter(todo => !todo.is_completed);
         }
     },
     ifEquals: (arg1, arg2, options) => {
