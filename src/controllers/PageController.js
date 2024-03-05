93418d21971ea453a3b64b21ac30e4d949947472
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-03-05 23:34:08 +0100

    Added category information to todos in PageController and todo.hbs

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -3,7 +3,7 @@ import Todo from '../models/Todo.js';
 import Category from '../models/Category.js';
 
 export const todos = async (req, res) => {
-    const todos = await Todo.query();
+    const todos = await Todo.query().withGraphFetched('category');
     const categoriesData = await Category.query();
     const data = {
         todos: todos,
@@ -18,7 +18,7 @@ export const categorizedTodos = async (req, res) => {
     const categoryParam = req.params.category;
     const category = await Category.query().whereRaw('LOWER(name) = ?', [categoryParam.toLowerCase()]).first();
     
-    const todos = await Todo.query();
+    const todos = await Todo.query().withGraphFetched('category');
     const categoriesData = await Category.query();
     const data = {
         todos: !category ? todos : todos.filter(todo => todo.category_id === category.id),
