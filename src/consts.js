852a6f797d11c5d23dc9d86a5e2adaf5c7118283
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-03-18 11:13:24 +0100

    Updated consts.js to remove default values for DATABASE_TYPE, DATABASE_NAME, and NODE_ENV

--- src/consts.js
+++ src/consts.js
@@ -6,6 +6,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
-export const DATABASE_NAME = process.env.DATABASE_NAME || "just-do-it.sqlite3";
-export const NODE_ENV = process.env.NODE_ENV || "development";
+export const DATABASE_TYPE = process.env.DATABASE_TYPE;
+export const DATABASE_NAME = process.env.DATABASE_NAME;
+export const NODE_ENV = process.env.NODE_ENV;
